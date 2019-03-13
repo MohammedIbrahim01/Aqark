bac5fff1f2852f7a52a75cc0647930fa704f1722
Abdelaziz <Abdelaziz.elantaky@Gmail.com>
2019-03-13 23:31:33 +0200

    creation of recycler view

--- app/src/main/java/com/abdelazim/x/aqark/favorites/FavoritesFragment.java
+++ app/src/main/java/com/abdelazim/x/aqark/favorites/FavoritesFragment.java
@@ -2,7 +2,10 @@ package com.abdelazim.x.aqark.favorites;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +17,7 @@ import com.abdelazim.x.aqark.R;
  * A simple {@link Fragment} subclass.
  */
 public class FavoritesFragment extends Fragment {
-
+    RecyclerView favouriteRecyclerView;
 
     public FavoritesFragment() {
         // Required empty public constructor
@@ -28,4 +31,14 @@ public class FavoritesFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_favourites, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViews(view);
+    }
+
+
+    private void initViews(View view) {
+        favouriteRecyclerView = view.findViewById(R.id.favouritesRecyclerView);
+    }
 }
