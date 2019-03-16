2d9d8e6423a54a997b15d0859528c2a413c2afb9
Abdelaziz <Abdelaziz.elantaky@Gmail.com>
2019-03-16 13:19:53 +0200

    addition of implementatin inside favoirites fragements'

--- app/src/main/java/com/abdelazim/x/aqark/favorites/FavoritesFragment.java
+++ app/src/main/java/com/abdelazim/x/aqark/favorites/FavoritesFragment.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.abdelazim.x.aqark.R;
+import com.abdelazim.x.aqark.favorites.model.ApartmentDetails;
+import com.abdelazim.x.aqark.favorites.model.ApartmentDetailsAdapter;
+
+import java.util.List;
 
 
 /**
@@ -18,11 +23,12 @@ import com.abdelazim.x.aqark.R;
  */
 public class FavoritesFragment extends Fragment {
     RecyclerView favouriteRecyclerView;
+    ApartmentDetailsAdapter apartmentDetailsAdapter;
+    List<ApartmentDetails> apartmentDetailsList;
 
     public FavoritesFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,10 +41,23 @@ public class FavoritesFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         initViews(view);
+        retrieveFavorites();
+        favouriteRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        favouriteRecyclerView.setHasFixedSize(true);
+        favouriteRecyclerView.setAdapter(apartmentDetailsAdapter);
+    }
+
+    private void retrieveFavorites() {
+        //temporary cause there is no data source.
+        apartmentDetailsList=apartmentDetailsAdapter.getApartmentDetailsList();
+
+        apartmentDetailsAdapter.notifyDataSetChanged();
+        apartmentDetailsAdapter.setApartmentDetailsList(apartmentDetailsList);
     }
 
 
     private void initViews(View view) {
         favouriteRecyclerView = view.findViewById(R.id.favouritesRecyclerView);
+        apartmentDetailsAdapter= new ApartmentDetailsAdapter();
     }
 }
